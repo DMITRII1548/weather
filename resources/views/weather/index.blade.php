151eeef496807c377196aede6257515b84e3f27c
Dmitrii <korsunov2006@list.ru>
2022-10-11 19:59:15 +0600

    view index corrected

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.weather')
 
 @section('content')
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">City</th>
-              </tr>
-            </thead>
-            @foreach ($cities as $city)
-              <tbody>
-                <tr>
-                  <th scope="row">{{ ++$number }}</th>
-                  <td><a href="{{ route('weather.show', $city->id) }}">{{ $city->city }}</a></td>
-                </tr>
-              </tbody>
-            @endforeach
-          </table>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+        <th scope="col">City</th>
+          </tr>
+          </thead>
+  @foreach ($cities as $city)
+    <tbody>
+      <tr>
+        <th scope="row">{{ ++$number }}</th>
+        <td><a href="{{ route('weather.show', $city->id) }}">{{ $city->city }}</a></td>
+      </tr>
+    </tbody>
+  @endforeach
+</table>
 @endsection
