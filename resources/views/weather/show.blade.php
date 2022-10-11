a4c0d490e0802746581f20f09107a4b8d551ac85
Dmitrii <korsunov2006@list.ru>
2022-10-11 19:05:10 +0600

    button back added

--- resources/views/weather/show.blade.php
+++ resources/views/weather/show.blade.php
@@ -13,7 +13,8 @@
     <h1>Weather in {{ $city }} Now:</h1><br>
     <h3>Weather: {{ $weather }}</h3>
     <h3>Temperature: {{ $temperature }} °C</h3>
-    <h3>Wind Speed: {{ $windSpeed }} m/s</h3>
+    <h3>Wind Speed: {{ $windSpeed }} m/s</h3><br>
+    <a href="{{ route('weather.index') }}" class="btn btn-outline-primary btn-lg">Back</a>
 </div>    
 </body>
 </html>
