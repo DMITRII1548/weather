26c09f61df01131f466b546eb0c415ca15810c26
Dmitrii <korsunov2006@list.ru>
2022-10-11 19:54:59 +0600

    layout weather added

--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/js/app.js'])
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
+@extends('layouts.weather')
+
+@section('content')
         <table class="table">
             <thead>
               <tr>
@@ -25,6 +17,4 @@
               </tbody>
             @endforeach
           </table>
-    </div>
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::get('/weather', IndexContoller::class)->name('weather.index');
 Route::get('/weather/{city}', ShowContorller::class)->name('weather.show');
 
 
-/*Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-*/
