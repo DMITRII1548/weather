59fd9546002d13ff98add91bc85d8a424a2fc2be
Dmitrii <korsunov2006@list.ru>
2022-10-12 20:11:07 +0600

    view create modified

--- app/Http/Controllers/Weather/StoreController.php
+++ app/Http/Controllers/Weather/StoreController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Weather;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Weather\StoreRequest;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
     
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        dd('store');
+        $data = $request->validated();
+        dd($data);
     }
 
 }
--- resources/views/weather/create.blade.php
+++ resources/views/weather/create.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.weather')
 
 @section('content')
-    <h1>Create</h1>
+<div class="mt-4">
+    <form action="{{ route('weather.store') }}" method="post">
+    @csrf
+        <label for="City" class="form-label">City</label>
+        <input type="text" id="City" class="form-control" placeholder="City" name="sity">
+        <div id="City" class="form-text">
+            Your city's name must start with a large letter, be real and your city must be in Kazakhstan
+        </div>
+        <button type="submit" class="btn btn-outline-primary mt-3">Create</button>
+    </form>
+</div>
 @endsection
